33c8e93092c31bf6e4c817a689f075301ecf78ac
janesummer <1752321720@qq.com>
2020-05-24 13:31:15 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -8,14 +8,27 @@ const user = {
       pathname: '',
       timer: null,
       timer2: null,
-      timer3: null
+      timer3: null,
+      timer4: null,
+      first: true
     }
   },
   created () {
     let pathname = window.location.pathname.match(/[^\/text\/]+/)[0];
     this.pathname = pathname;
-    this.timer = setInterval(() => {
-      axios.get(`https://www.chiens.cn/getText/${pathname}`)
+    axios.get(`https://www.chiens.cn/getText/${pathname}`)
+    .then((res) => {
+      console.log(res)
+      this.text = res.data.data;
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  },
+  mounted() {
+    this.timer2 = setInterval(() => {
+      console.log('sss')
+      axios.get(`https://www.chiens.cn/getText/${this.pathname}`)
       .then((res) => {
         console.log(res)
         this.text = res.data.data;
@@ -24,42 +37,46 @@ const user = {
         console.log(err)
       })
     }, 2000);
-    this.timer2 = setTimeout(() => {
-      this.timer3 = setInterval(() => {
+    this.timer3 = setTimeout(() => {
+      this.timer4 = setInterval(() => {
         clearInterval(this.timer)
       }, 500);
       setTimeout(() => {
-        clearInterval(this.timer3)
+        clearInterval(this.timer4)
       }, 5000)
-      clearTimeout(this.timer2)
+      clearTimeout(this.timer3)
     }, 300000)
   },
   watch: {
     'text': {
       handler (now, old) {
-        clearTimeout(this.timer);
-        this.timer = null;
-        this.timer = setTimeout(() => {
-          console.log('更新', now);
-          let param = new URLSearchParams();
-          param.append('data', now)
-          axios({
-            url: `https://www.chiens.cn/getText/write?id=${this.pathname}`,
-            method: 'post',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: param
-          })
-          .then((res) => {
-            console.log(res)
-          })
-          .catch((err) => {
-            console.log(err)
-          })
+        if (!this.first) {
           clearTimeout(this.timer);
           this.timer = null;
-        }, 1000);
+          this.timer = setTimeout(() => {
+            console.log('更新', now);
+            let param = new URLSearchParams();
+            param.append('data', now)
+            axios({
+              url: `https://www.chiens.cn/getText/write?id=${this.pathname}`,
+              method: 'post',
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+              },
+              data: param
+            })
+            .then((res) => {
+              console.log(res)
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+            clearTimeout(this.timer);
+            this.timer = null;
+          }, 1000);
+        } else {
+          this.first = false
+        }
       },
       deep: true
     }

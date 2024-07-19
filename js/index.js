2496730ecff8cab4890d63a98eb232dde8e6703e
Janesummer <1752321720@qq.com>
2024-07-19 17:14:33 +0800

    ✨ feat:修改

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // new VConsole();
 const user = {
-  template: `<textarea id="text" v-model="text"></textarea>`,
+  template: `
+    <textarea id="text" v-model="text"></textarea>
+  `,
   data () {
     return {
       text: '',
@@ -11,6 +13,7 @@ const user = {
     }
   },
   created () {
+    console.log('0d0s0d');
     let pathname = window.location.pathname.match(/[^\/text\/]+/)[0];
     this.pathname = pathname;
     axios.get(`https://www.chiens.cn/getText/${pathname}`)
@@ -21,6 +24,15 @@ const user = {
     .catch((err) => {
       console.log(err)
     })
+  },
+  methods: {
+    toClipboard() {
+      // return navigator.clipboard.readText().then(res => {
+      //   console.log('剪贴板内容', res);
+      //   Qmsg.success("粘贴成功", { autoClose: true, onClose: () => {} });
+      //   return res
+      // })
+    }
   },
   watch: {
     'text': {
@@ -76,7 +88,26 @@ let vm = new Vue({
   el: '#app',
   router,
   created () {
-    if (window.location.pathname == '/text/') {
+    // axios.post('https://www.chiens.cn/getText')
+    // .then((res) => {
+      
+    //   // window.location = `${href}${res.data.data}`
+    // })
+
+    axios({
+      url: "https://www.chiens.cn/getText/0a7A4",
+      method: "POST",
+      data: {},
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }).then(res => {
+      console.log(res)
+      let href = window.location.href;
+      console.log('hhh', `${href}${res.data.data}`);
+    })
+    
+    if (window.location.pathname === '/text/') {
       axios.get('https://www.chiens.cn/getText')
       .then((res) => {
         console.log(res)
